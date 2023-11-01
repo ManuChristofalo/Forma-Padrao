38e3e1c78e4ebd926e45feb7e47fd38abf7c35d1
Manuele S. Christófalo <102175989+ManuChristofalo@users.noreply.github.com>
2023-11-01 11:05:54 -0300

    Arquivos Atualizados

--- Simplex.js
+++ Simplex.js
@@ -1,6 +1,7 @@
 
 // 1. MUDANÇA NO NÚMERO DE VARIÁVEIS =======================================================================================
-var countRestr=1;
+var numRestr=1;
+var numVariaveis=2;
 
 document.getElementById('numVariaveis').addEventListener('change', atualizaCampos);
 atualizaCampos();
@@ -9,7 +10,7 @@ function atualizaCampos(){
     document.getElementById('funcaoObjetivo').innerHTML = ''; //Limpeza dos campos a cada alteração
     document.getElementById('restricoes').innerHTML = '';
 
-    countRestr=0;
+    numRestr=0;
 
     adicionaObjetivo();
     adicionaRestricao();
@@ -19,7 +20,7 @@ function atualizaCampos(){
 // 2. FUNÇÕES DE DINAMICIDADE =========================================================================================
 
 function adicionaObjetivo(){
-    const numVariaveis = parseInt(document.getElementById('numVariaveis').value) || 2;
+    numVariaveis = parseInt(document.getElementById('numVariaveis').value) || 2;
 
     // I. Minimizar ou Maximizar 
         const minMax = document.createElement('select');
@@ -51,24 +52,21 @@ function adicionaObjetivo(){
                 span.textContent = 'x' + i + '  +  ';
                 document.getElementById('funcaoObjetivo').appendChild(span);
             }
-
-            else if(i==numVariaveis){
-                const span = document.createElement('span');
-                span.textContent = 'x' + i + ' = ';
-                document.getElementById('funcaoObjetivo').appendChild(span);
-
-                const input = document.createElement('input');
-                input.id = "funcObjResult"
-                input.type = 'number';
-                input.className = 'resultado';
-                input.placeholder = `Resultado`;
-                document.getElementById('funcaoObjetivo').appendChild(input);
-            }
         }
 }
 
+function removeRestricao(){
+    if(numRestr>1){
+        const lastAddedDiv = document.getElementById('restricoes').lastElementChild;
+        if(lastAddedDiv){
+            lastAddedDiv.remove();
+            numRestr--;
+        }
+    }
+}
+
 function adicionaRestricao(){
-    if(countRestr<5){
+    if(numRestr<5){
         const numVariaveis = parseInt(document.getElementById('numVariaveis').value) || 2;
         const DivRestricao = document.createElement("div");
         
@@ -76,7 +74,7 @@ function adicionaRestricao(){
         // I. Coeficientes e Variáveis
             for(let i = 1; i <= numVariaveis; i++){
                 const input = document.createElement('input');
-                input.id = 'restrVar' + i + (countRestr+1); //i=coluna (xi), countRestr=linha
+                input.id = 'restrVar' + i + (numRestr+1); //i=coluna (xi), countRestr=linha
                 input.type = 'number';
                 input.className = 'coeficiente';
                 input.placeholder = `coef X${i}`;
@@ -99,7 +97,7 @@ function adicionaRestricao(){
         // II. Seleção do tipo de equação
             const select = document.createElement('select');
             select.className = 'sinalEq';
-            select.id = 'sinal' + (countRestr+1);
+            select.id = 'sinal' + (numRestr+1);
             const options = ['=', '≤', '≥'];
     
             for(let k = 0; k < options.length; k++){
@@ -114,7 +112,7 @@ function adicionaRestricao(){
     
         // III. Resultado
             const inputResultado = document.createElement('input');
-            inputResultado.id = 'restrResult' + (countRestr+1);
+            inputResultado.id = 'restrResult' + (numRestr+1);
             inputResultado.type = 'number';
             inputResultado.className = 'resultado';
             inputResultado.placeholder = 'Resultado';
@@ -122,7 +120,7 @@ function adicionaRestricao(){
     
     
         document.getElementById("restricoes").appendChild(DivRestricao);
-        countRestr++;
+        numRestr++;
     }
 }
 
@@ -131,7 +129,7 @@ function adicionaRestricao(){
 // 3. FUNÇÕES PARA SALVAR VALORES =========================================================================================
 
 const vetorObjetivo = []; //Bool Min/Max - Coef. Variaveis - Resultado
-let vetorRestricao =[]; //Coef. Variaveis - Resultado - Sinal (*numRestrições)
+let matrizRestricao =[]; //Coef. Variaveis - Resultado - Sinal (*numRestrições)
 
 
 function salvaValor(){
@@ -152,29 +150,24 @@ function salvaValor(){
     }
 
 
-    //III. Resultados Objetivo
-    let valor = parseInt(document.getElementById('funcObjResult').value);
-    vetorObjetivo.push(valor);
-
-
 
     //3.2 - RESTRIÇÕES --------------------------------------------------------------------
-    for(let i=0; i<countRestr; i++){
-        vetorRestricao[i]=new Array(numVariaveis+2);
+    for(let i=0; i<numRestr; i++){ //Matriz Restrição
+        matrizRestricao[i]=new Array(numVariaveis+2);
     }
 
-    for(let i = 1; i <=countRestr; i++){
+    for(let i = 1; i <=numRestr; i++){ //Colunas Coeficientes
         for(let j=1; j<=numVariaveis; j++){
             let valor = parseInt(document.getElementById('restrVar'+j+i).value);
-            vetorRestricao[i-1][j-1]=valor;
+            matrizRestricao[i-1][j-1]=valor;
         }
     }
 
-    for(let i=1; i<=countRestr; i++){
+    for(let i=1; i<=numRestr; i++){ //Colunas Resultados
         let valor = parseInt(document.getElementById('restrResult'+i).value);
-        vetorRestricao[i-1][numVariaveis]=valor;
+        matrizRestricao[i-1][numVariaveis]=valor;
 
-        var sinal;
+        var sinal; //Coluna Sinal
         if(document.getElementById('sinal'+i).value === "=")
             sinal=0;
         else if(document.getElementById('sinal'+i).value === "≥")
@@ -182,13 +175,98 @@ function salvaValor(){
         else
             sinal=-1;
     
-        vetorRestricao[i-1][numVariaveis+1]=sinal;
+        matrizRestricao[i-1][numVariaveis+1]=sinal;
     }
 
-    for(let i=0; i<countRestr; i++){
-        for(let j=0; j<numVariaveis+2; j++) console.log(vetorRestricao[i][j]);
-        console.log("\n");
+    criaMatrizFormaPadrao();
+    //Call Funções Simplex
+}
+
+
+
+//4. SIMPLEX ========================================================================================================================
+
+function criaMatrizFormaPadrao(){
+    //I. Criação da matriz já na forma padrão (tablo) - ver anexo matrizPadrao.png
+    let matrizPadrao = [];
+    for(let linha=0; linha<numRestr+2; linha++){ //num linhas -> num restrição + func. obj + CRs
+        matrizPadrao[linha]=new Array(numVariaveis+(2*numRestr)+2); //num colunas -> bases + num variaveis + variaveis auxiliares + resultado
     }
 
+    for(let linha=0; linha<numRestr+2; linha++){
+        for(let coluna=0; coluna<numVariaveis+(numRestr*2)+2; coluna++){
+            matrizPadrao[linha][coluna]=0;
+        }
+    }
 
+    //II. Preenchimento da matriz padrão
+    //Linha 0 - Função Objetivo
+    for(let coluna=1; coluna<=numVariaveis; coluna++){ //Variáveis normais
+        if(vetorObjetivo[0]===1)
+            matrizPadrao[0][coluna]=vetorObjetivo[coluna]*-1;
+        else
+            matrizPadrao[0][coluna]=vetorObjetivo[coluna];
+    }
+
+    let restricao=0;
+    for(let coluna=numVariaveis+1; coluna<numVariaveis+(numRestr*2)+1; coluna++){ //Variáveis de ajuste
+        console.log(matrizRestricao[restricao][numVariaveis+1]);
+        if(matrizRestricao[restricao][numVariaveis+1]===-1){ //Se a restrição for <=
+            matrizPadrao[0][coluna]=0;
+            coluna++;
+            matrizPadrao[0][coluna]=0;
+        }
+        else if(matrizRestricao[restricao][numVariaveis+1]===0 || matrizRestricao[restricao][numVariaveis+1]===1){ //Se a restrição for === ou >=
+            matrizPadrao[0][coluna]=0;
+            coluna++;
+            matrizPadrao[0][coluna]=10000;
+        }
+        restricao++;
+    }
+    
+
+    //Linha 2 até i-1 - Restrições
+    for(let linha=1; linha<numRestr+1; linha++){
+        for(let coluna=0; coluna<numVariaveis+(numRestr*2)+1; coluna++){
+            if(coluna==0){ //Coluna 0 - Valores das bases na f.obj. | Isso ta certo
+                if(matrizRestricao[linha-1][numVariaveis+1]==-1){ //Se a restrição for <=
+                    matrizPadrao[linha][0]=matrizPadrao[0][numVariaveis+2*linha-1];
+                }
+                else matrizPadrao[linha][0]=matrizPadrao[0][numVariaveis+2*linha];
+            }
+
+            else if(coluna==numVariaveis+2*linha-1 || coluna==numVariaveis+2*linha){ //Colunas var. aux. 
+                if(matrizRestricao[linha-1][numVariaveis+1]==-1){
+                    matrizPadrao[linha][numVariaveis+2*linha-1]=+1;
+                    matrizPadrao[linha][numVariaveis+2*linha]=0;
+                }
+                else if(matrizRestricao[linha-1][numVariaveis+1]==0){
+                    matrizPadrao[linha][numVariaveis+2*linha-1]=0;
+                    matrizPadrao[linha][numVariaveis+2*linha]=+1;
+                }
+                else{
+                    matrizPadrao[linha][numVariaveis+2*linha-1]=-1;
+                    matrizPadrao[linha][numVariaveis+2*linha]=+1;
+                }
+            }
+
+            else if(coluna>0 && coluna<=numVariaveis){ //Colunas var. normais | Isso ta certo
+                matrizPadrao[linha][coluna]=matrizRestricao[linha-1][coluna-1];
+            }
+        }
+    }
+
+    for(let linha=0; linha<numRestr+1; linha++){
+        var array=[];
+        for(let coluna=0; coluna<numVariaveis+(numRestr*2)+1; coluna++){
+            array[coluna]=matrizPadrao[linha][coluna];
+        }
+        var str=array.join(' ');
+        console.log(str);
+    }
+
+    console.log("\n");
+    for(coluna=0; coluna<numVariaveis+(numRestr*2)+1; coluna++){
+        console.log(matrizPadrao[1][coluna]);
+    }
 }
